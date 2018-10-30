bfddd50443004a5fc9cfb8e537709c9a1967d155
EC2 Default User <ec2-user@ip-172-31-1-125.us-east-2.compute.internal>
2018-10-30 09:54:50 +0000

    kg_app_1030

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,13 +5,21 @@ class ChatsController < ApplicationController
   
   def show
       
-      @chats = Chat.where("(sender_id = ?,receiver_id = ?) OR (sender_id = ?,receiver_id = ?)",
-                            current_user.id,params[:id],params[:id],current_user.id).order(created_at: :desc)
-
+      @chats = Chat.where("(sender_id = ? and receiver_id = ?) OR (sender_id = ? and receiver_id = ?)",
+                            @current_user.id,params[:id],params[:id],@current_user.id)
+      @friend_id = params[:id]
+      
   end
   
   def create
-     
+      @chat = Chat.new(
+      message: params[:message],
+      sender_id: @current_user.id,
+      receiver_id: params[:receiver_id]
+      )
+      
+    @chat.save
+    redirect_to("/chats/#{params[:receiver_id]}")
 
        
   end
