04e622262797bd911c35cfbd78ad5a108be2890d
EC2 Default User <ec2-user@ip-172-31-1-125.us-east-2.compute.internal>
2018-11-06 10:22:54 +0000

    kg_app11_6_2

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -24,12 +24,36 @@ class ChatsController < ApplicationController
        
   end
   
-  def index
+   def index
     @chats = Chat.where("(sender_id = ? ) OR (receiver_id = ?)",
-                            @current_user.id,@current_user.id)
-    @friends = @chats.find_all {|k,v| v !=  @current_user.id }
-    @friend = @friends.to_h
+                            @current_user.id,@current_user.id).order(created_at: :desc)
+    
+    if @chats.present?
+       friend_chat = []
+       @chat_message = []
+       
+       @chats.each do |chat| 
+           if chat.sender_id != @current_user.id
+               if !friend_chat.include?(chat.sender_id)
+                 friend_chat.push(chat.sender_id)
+                 logger.debug("=========================== friend chat list 上 = #{chat.sender_id}..#{chat.message}")
+                 @chat_message.push([chat.sender_id,chat.message])
+               end
+           else
+               if  !friend_chat.include?(chat.receiver_id)
+                 friend_chat.push(chat.receiver_id)
+                 logger.debug("=========================== friend chat list 下 = #{chat.receiver_id}..#{chat.message}")
+                 @chat_message.push([chat.receiver_id,chat.message])
+               end
+           end
+         end
+    end
+       
+       
+       
+        # @friends = @chats.find_all {|k,v| v !=  @current_user.id }
+        # @friend = @friends.to_h
 
-  end
+   end
   
 end
